5b9a3096b47dbc69022930fba001a2b8f08da5f3
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:02:50 +0300

    Add project files.

---
+++ Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        // •	Задача 3: Напишите программу, которая выводит все четные числа от 1 до 100.
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<101; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
